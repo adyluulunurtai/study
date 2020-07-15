543c15b29044dcc3697673eb750aab74f6f4ca06
Nurtai <adyluulunurtai7@gmail.com>
2020-07-15 14:19:24 +0600

    second commit

--- script.js
+++ script.js
@@ -10,22 +10,40 @@ let personalMovieDB = {
   privat: false,
 };
 
-const a = prompt("odin iz poslednix filmov kotorye vy posmotreli", ""),
-  b = prompt("naskolko ocenili ego", ""),
-  c = prompt("odin iz poslednix filmov kotorye vy posmotreli", ""),
-  d = prompt("naskolko ocenili ego", "");
+while (personalMovieDB.movies < 2) {
+  let a = prompt("odin iz poslednix filmov kotorye vy posmotreli", ""),
+    b = prompt("naskolko ocenili ego", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-const num = 50;
-switch (num) {
-  case 49:
-    console.log("neverno");
-    break;
-  case 50:
-    console.log("v to4ku");
-    break;
-  default:
-    console.log("ne v etot raz");
+  if (a != null && b != null && a != "" && b != "" && a.length < 50 && b.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("done");
+  } else {
+    i--
+    console.log('error');
+  }
 }
+
+
+/*for (let i = 0; i < 2; i++) {
+  let a = prompt("odin iz poslednix filmov kotorye vy posmotreli", ""),
+    b = prompt("naskolko ocenili ego", "");
+
+  if (a != null && b != null && a != "" && b != "" && a.length < 50 && b.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("done");
+  } else {
+    i--
+    console.log('error');
+  }
+
+}
+
+if (personalMovieDB.count < 10) {
+  console.log("prosmotreno dovolno malo filmov");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  console.log("vy kinoman");
+} else {
+  console.log("ty super")
+}
+*/
+console.log(personalMovieDB);
